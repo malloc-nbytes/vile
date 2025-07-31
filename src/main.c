d21b905ad1915c392c1e3acb6e0e5d460352751a
malloc-nbytes <zdhdev@yahoo.com>
2025-07-31 17:26:08 -0500

    filepath no longer required

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@
 
 #include <forge/io.h>
 #include <forge/arg.h>
-#include <forge/arg.h>
 #include <forge/lexer.h>
 #include <forge/str.h>
 #include <forge/rdln.h>
@@ -65,8 +64,7 @@ lex_file(const char *fp)
 }
 
 void
-run(str_array *input_lines,
-    str_array *user_lines)
+run(str_array *input_lines, str_array *user_lines)
 {
         str_array final_lines = dyn_array_empty(str_array);
         for (size_t i = 0; i < input_lines->len; ++i) {
@@ -79,39 +77,47 @@ run(str_array *input_lines,
         forge_io_create_file(TMP_FILEPATH, 1);
         forge_io_write_lines(TMP_FILEPATH, (const char **)final_lines.data, final_lines.len);
 
-        const char *basename = forge_io_basename(g_config.fp);
+        if (g_config.fp) {
+                const char *basename = forge_io_basename(g_config.fp);
 
-        char *cc_cmd = forge_str_builder("cc -c ", g_config.fp, " -o /tmp/", basename, ".o", NULL);
-        CMD(cc_cmd, {
-                free(cc_cmd);
-                goto bad;
-        });
-        char *ar_cmd = forge_str_builder("ar rcs /tmp/lib", basename, ".a /tmp/", basename, ".o", NULL);
-        CMD(ar_cmd, {
-                free(cc_cmd);
-                free(ar_cmd);
-                goto bad;
-        });
-        char *cc2_cmd = forge_str_builder("cc " TMP_FILEPATH " -o /tmp/ViLe_bin_output -L/tmp/ -l", basename, NULL);
-        CMD(cc2_cmd, {
+                char *cc_cmd = forge_str_builder("cc -c ", g_config.fp, " -o /tmp/", basename, ".o", NULL);
+                CMD(cc_cmd, {
+                        free(cc_cmd);
+                        goto bad;
+                });
+                char *ar_cmd = forge_str_builder("ar rcs /tmp/lib", basename, ".a /tmp/", basename, ".o", NULL);
+                CMD(ar_cmd, {
+                        free(cc_cmd);
+                        free(ar_cmd);
+                        goto bad;
+                });
+                char *cc2_cmd = forge_str_builder("cc " TMP_FILEPATH " -o /tmp/ViLe_bin_output -L/tmp/ -l", basename, NULL);
+                CMD(cc2_cmd, {
+                        free(cc2_cmd);
+                        free(cc_cmd);
+                        free(ar_cmd);
+                        goto bad;
+                });
+
                 free(cc2_cmd);
                 free(cc_cmd);
                 free(ar_cmd);
-                goto bad;
-        });
-
-        free(cc2_cmd);
-        free(cc_cmd);
-        free(ar_cmd);
+        } else {
+                char *cc_cmd = forge_str_builder("cc " TMP_FILEPATH " -o /tmp/ViLe_bin_output", NULL);
+                CMD(cc_cmd, {
+                        free(cc_cmd);
+                        goto bad;
+                });
+                free(cc_cmd);
+        }
 
         CMD("/tmp/ViLe_bin_output", goto bad);
         goto cleanup;
 
- bad:
+bad:
         printf("aborting...\n");
 
- cleanup:
-
+cleanup:
         dyn_array_free(final_lines);
 }
 
@@ -124,10 +130,6 @@ handle_args(forge_arg *it)
                 }
                 it = it->n;
         }
-
-        if (!g_config.fp) {
-                forge_err("a filepath is needed");
-        }
 }
 
 int
@@ -137,15 +139,19 @@ main(int argc, char **argv)
         handle_args(arghd);
         forge_arg_free(arghd);
 
-        forge_lexer lexer = lex_file(g_config.fp);
+        forge_lexer lexer = {0};
+        str_array input_lines = dyn_array_empty(str_array);
 
-        str_array input_lines = parse(&lexer);
+        if (g_config.fp) {
+                lexer = lex_file(g_config.fp);
+                input_lines = parse(&lexer);
 
-        printf("Available Interface:\n");
-        for (size_t i = 0; i < input_lines.len; ++i) {
-                forge_str line = forge_str_from(input_lines.data[i]);
-                char *s = forge_str_contains_substr(&line, " ", 0);
-                if (s) printf("%s\n", s);
+                printf("Available Interface:\n");
+                for (size_t i = 0; i < input_lines.len; ++i) {
+                        forge_str line = forge_str_from(input_lines.data[i]);
+                        char *s = forge_str_contains_substr(&line, " ", 0);
+                        if (s) printf("%s\n", s);
+                }
         }
 
         while (1) {
@@ -154,7 +160,7 @@ main(int argc, char **argv)
 
                 while (1) {
                         char *input = forge_rdln("ViLe: ");
-                        if (!strcmp(input, ":r") || !strcmp(input, ":R")) {
+                        if (!strcmp(input, ":r") || !strcmp(input, ":run")) {
                                 free(input);
                                 break;
                         } else if (!strcmp(input, ":i")) {
@@ -162,9 +168,16 @@ main(int argc, char **argv)
                                 input = forge_rdln("#include ");
                                 dyn_array_append(input_lines, forge_str_builder("#include ", input, NULL));
                                 free(input);
-                        } else if (!strcmp(input, ":q")) {
+                        } else if (!strcmp(input, ":q") || !strcmp(input, ":quit")) {
                                 free(input);
                                 goto done;
+                        } else if (!strcmp(input, ":h") || !strcmp(input, ":help")) {
+                                printf("Commands:\n");
+                                printf("  :r, :run   - Run the code\n");
+                                printf("  :i         - Add an include directive\n");
+                                printf("  :q, :quit  - Quit the program\n");
+                                printf("  :h, :help  - Show this help message\n");
+                                free(input);
                         } else if (input) {
                                 dyn_array_append(user_lines, input);
                         }
@@ -180,8 +193,13 @@ main(int argc, char **argv)
                 dyn_array_free(user_lines);
         }
 
- done:
-        forge_lexer_destroy(&lexer);
-
+done:
+        if (g_config.fp) {
+                forge_lexer_destroy(&lexer);
+                for (size_t i = 0; i < input_lines.len; ++i) {
+                        free(input_lines.data[i]);
+                }
+                dyn_array_free(input_lines);
+        }
         return 0;
 }
